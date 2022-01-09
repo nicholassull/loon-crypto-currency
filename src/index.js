d832771ff3030021bb338b5eb5d30995e834f2a8
Nick Sullivan <nicholassull@gmail.com>
2022-01-09 10:41:49 -0800

    add initial files
    Co-authored-by: Erin McCulley <ejmcculley@gmail.com>

---
+++ src/index.js
@@ -0,0 +1,3 @@
+https://api.coingecko.com/api/v3/ping
+
+https://api.coingecko.com/api/v3/simple/price?ids=Algorand&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true
