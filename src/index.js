ed505e9f5214eb71f49f5ace143a644d212f4f7d
Nick Sullivan <nicholassull@gmail.com>
2022-01-09 15:46:21 -0800

    added text, background, and other page styling along with currency information
    Co-authored-by: Erin McCully <ejmcculley@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,18 +1,55 @@
 import $ from 'jquery';
 import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
 import CryptoService from './crypto-service.js';
 
 
 
+
 $(document).ready(function() {
-  $("#test").click(function() {
+  $("#enter").click(function(){
+    $("#crypto-display").show();
+    $("#splash").hide();
+  })
+  $("#trade").click(function() {
+    $("#trade-display").show();
+    $("#crypto-display").hide();
+  })
+  $("#back").click(function() {
+    $("#crypto-display").show();
+    $("#trade-display").hide();
+  })
+
+
+
+  $("#enter").click(function() {
     let promise = CryptoService.getCrypto()
     promise.then(function(response) {
       const body = JSON.parse(response);
-      $("#bitcoin").text(`Bitcoin Price in USD:  ${body.bitcoin.usd}`);
-      $("#ethereum").text(`Ethereum Price in USD:  ${body.ethereum.usd}`);
-      $("#dogecoin").text(`Dogecoin Price in USD:  ${body.dogecoin.usd}`);
-      $("#algorand").text(`Algorand Price in USD:  ${body.algorand.usd}`);
+      $("#bitcoin-price").text(`$${body.bitcoin.usd}`);
+      $("#bitcoin-change").text(`${body.bitcoin.usd_24h_change.toFixed(3)}%`);
+      $("#bitcoin-volume").text(`$${Math.round(body.bitcoin.usd_24h_vol)}`);
+      $("#bitcoin-cap").text(`$${Math.round(body.bitcoin.usd_market_cap)}`);
+
+      $("#ethereum-price").text(`$${body.ethereum.usd}`);
+      $("#ethereum-change").text(`${body.ethereum.usd_24h_change.toFixed(3)}%`);
+      $("#ethereum-volume").text(`$${Math.round(body.ethereum.usd_24h_vol)}`);
+      $("#ethereum-cap").text(`$${Math.round(body.ethereum.usd_market_cap)}`);
+
+      $("#dogecoin-price").text(`$${body.dogecoin.usd}`);
+      $("#dogecoin-change").text(`${body.dogecoin.usd_24h_change.toFixed(3)}%`);
+      $("#dogecoin-volume").text(`$${Math.round(body.dogecoin.usd_24h_vol)}`);
+      $("#dogecoin-cap").text(`$${Math.round(body.dogecoin.usd_market_cap)}`);
+
+      $("#algorand-price").text(`$${body.algorand.usd}`);
+      $("#algorand-change").text(`${body.algorand.usd_24h_change.toFixed(3)}%`);
+      $("#algorand-volume").text(`$${Math.round(body.algorand.usd_24h_vol)}`);
+      $("#algorand-cap").text(`$${Math.round(body.algorand.usd_market_cap)}`);
+
+      $("#ethereum-info").text(`Price in USD:  $${body.ethereum.usd}`);
+      $("#dogecoin-info").text(`Price in USD:  $${body.dogecoin.usd}`);
+      $("#algorand-info").text(`Price in USD:  $${body.algorand.usd}`);
     }, function(error) {
       $("#showError").text(`Oops! There was the following error: ${error}`);
     });
